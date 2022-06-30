d4cf83aeecb871b99c1e33126f969d194f6aec56
Aluno <aluno@ifslab.edu.br>
2022-06-30 08:50:23 -0300

    first commit

---
+++ src/main/java/com/devsuperior/work/DscrudWorkApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscrudWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscrudWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/work/DscrudWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscrudWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
